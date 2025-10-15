6731cc3bc92d01442868ad6ec1805bd902f58fa7
Kamil <mathey.academy@gmail.com>
2025-10-15 22:06:58 +0200

    feat(eslint,frontend): eslint fix, frontend variables
    Fix eslint config.
    Add sass with 7:1 architecture, setup reuseable variables
    based on design system.

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,13 @@ import prettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   prettier,
   {
-    ignores: ['node_modules', 'dist', '**/dist/**', '**/build/**'],
+    ignores: ['node_modules', 'dist', '**/dist/**', '**/build/**', '**/*.scss', 'eslint.config.js'],
     plugins: {
       'simple-import-sort': simpleImportSort,
       'unused-imports': unusedImports,
@@ -20,6 +20,36 @@ export default [
       'unused-imports/no-unused-imports': 'error',
       'simple-import-sort/imports': 'warn',
       'simple-import-sort/exports': 'warn',
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+        URL: 'readonly',
+      },
+    },
+  },
+
+  {
+    files: ['apps/frontend/src/**/*.ts', 'apps/frontend/src/**/*.tsx', 'apps/backend/src/**/*.ts'],
+    ignores: [],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+        URL: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommendedTypeChecked.rules,
     },
   },
 ];
