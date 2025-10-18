b5d1951cc8809cb30ef508e66e43c99c0940cf04
Kamil <mathey.academy@gmail.com>
2025-10-18 13:32:08 +0200

    feat(config): strongly typed config with zod

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppConfigModule } from './modules/config/config.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AppConfigModule, PrismaModule, UsersModule],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { EnvService } from './modules/config/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(EnvService);
+
+  await app.listen(config.get('PORT'));
 }
 bootstrap();
